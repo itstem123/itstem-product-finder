81ce858ea70cc8a455890179649fa368d0332d2c
itstem123 <denroa123123@gmail.com>
2026-08-16 19:52:18 +0900

    Add files via upload

---
+++ products.js
@@ -0,0 +1,82 @@
+window.PRODUCTS = [
+  {
+    "internal_no": 1,
+    "smartstore_product_no": "13685511894",
+    "name": "업소용 스텐 주방 조리대 선반 식당 테이블 60x40x80 2단 1인당 1개 한정",
+    "image": "images/13685511894.jpg",
+    "url": "https://smartstore.naver.com/itstem123123/products/13685511894",
+    "keywords": "업소용 스텐 주방 조리대 선반 식당 테이블 60x40x80 2단 1인당 1개 한정"
+  },
+  {
+    "internal_no": 2,
+    "smartstore_product_no": "13700267842",
+    "name": "낚시 릴 감기기 라인 와인더 감기 도구 릴줄 정리 용품 업그레이드 세트 [미터 카운터 포함]",
+    "image": "images/13700267842.jpg",
+    "url": "https://smartstore.naver.com/itstem123123/products/13700267842",
+    "keywords": "낚시 릴 감기기 라인 와인더 감기 도구 릴줄 정리 용품 업그레이드 세트 [미터 카운터 포함]"
+  },
+  {
+    "internal_no": 3,
+    "smartstore_product_no": "13700268351",
+    "name": "서버랙 벽걸이형 네트워크 랙 4 U 디럭스 530x400x240 벽걸이형 바닥형",
+    "image": "images/13700268351.jpg",
+    "url": "https://smartstore.naver.com/itstem123123/products/13700268351",
+    "keywords": "서버랙 벽걸이형 네트워크 랙 4 U 디럭스 530x400x240 벽걸이형 바닥형"
+  },
+  {
+    "internal_no": 4,
+    "smartstore_product_no": "13701069188",
+    "name": "윤활유 내마모 테스트기 연료 기름 구리스 공업소 280W 디지털",
+    "image": "images/13701069188.jpg",
+    "url": "https://smartstore.naver.com/itstem123123/products/13701069188",
+    "keywords": "윤활유 내마모 테스트기 연료 기름 구리스 공업소 280W 디지털"
+  },
+  {
+    "internal_no": 5,
+    "smartstore_product_no": "13701070024",
+    "name": "야외 베란다 철제 방수 청소도구 정리함",
+    "image": "images/13701070024.jpg",
+    "url": "https://smartstore.naver.com/itstem123123/products/13701070024",
+    "keywords": "야외 베란다 철제 방수 청소도구 정리함"
+  },
+  {
+    "internal_no": 6,
+    "smartstore_product_no": "13701070612",
+    "name": "야외 테라스 정원 테이블 세트 플라스틱 우드 휴게 공간용",
+    "image": "images/13701070612.jpg",
+    "url": "https://smartstore.naver.com/itstem123123/products/13701070612",
+    "keywords": "야외 테라스 정원 테이블 세트 플라스틱 우드 휴게 공간용"
+  },
+  {
+    "internal_no": 7,
+    "smartstore_product_no": "13702375692",
+    "name": "산업용 미스트 선풍기 가습 냉풍기 강력 수냉식 업소용 야외용",
+    "image": "images/13702375692.jpg",
+    "url": "https://smartstore.naver.com/itstem123123/products/13702375692",
+    "keywords": "산업용 미스트 선풍기 가습 냉풍기 강력 수냉식 업소용 야외용"
+  },
+  {
+    "internal_no": 8,
+    "smartstore_product_no": "13707333359",
+    "name": "플라스틱 대형 직사각형 수납함 뚜껑 포함 물류용 박스 755 박스 [815x570x505 mm] 옐로우 뚜껑 없음",
+    "image": "images/13707333359.jpg",
+    "url": "https://smartstore.naver.com/itstem123123/products/13707333359",
+    "keywords": "플라스틱 대형 직사각형 수납함 뚜껑 포함 물류용 박스 755 박스 [815x570x505 mm] 옐로우 뚜껑 없음"
+  },
+  {
+    "internal_no": 9,
+    "smartstore_product_no": "13720636322",
+    "name": "회전 서가 원형 흰색 선반 슬라이딩 회전책장 잡지 공간분리 원목업그레이드카멜 3단 확대 배송입",
+    "image": "images/13720636322.jpg",
+    "url": "https://smartstore.naver.com/itstem123123/products/13720636322",
+    "keywords": "회전 서가 원형 흰색 선반 슬라이딩 회전책장 잡지 공간분리 원목업그레이드카멜 3단 확대 배송입"
+  },
+  {
+    "internal_no": 10,
+    "smartstore_product_no": "13720639918",
+    "name": "업소용 닭털 제거기계 자동 제모기 세척 처리장비 분무형 전동 깃털 탈모기 40L스탠다드핏 탈비둘기 탈계불가",
+    "image": "images/13720639918.jpg",
+    "url": "https://smartstore.naver.com/itstem123123/products/13720639918",
+    "keywords": "업소용 닭털 제거기계 자동 제모기 세척 처리장비 분무형 전동 깃털 탈모기 40L스탠다드핏 탈비둘기 탈계불가"
+  }
+];
