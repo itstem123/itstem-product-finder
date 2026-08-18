d41558bbdd1a0394e9682f97f6adb243a9c2166c
itstem123 <denroa123123@gmail.com>
2026-08-18 22:00:44 +0900

    Add files via upload

--- products.js
+++ products.js
@@ -5,7 +5,8 @@ window.PRODUCTS = [
     "name": "업소용 스텐 주방 조리대 선반 식당 테이블 60x40x80 2단 1인당 1개 한정",
     "image": "images/13685511894.jpg",
     "url": "https://smartstore.naver.com/itstem123123/products/13685511894",
-    "keywords": "업소용 스텐 주방 조리대 선반 식당 테이블 60x40x80 2단 1인당 1개 한정"
+    "keywords": "업소용 스텐 주방 조리대 선반 식당 테이블 60x40x80 2단 1인당 1개 한정",
+    "coupang_url": "https://www.coupang.com/vp/products/9507861465?vendorItemId=95845603087"
   },
   {
     "internal_no": 2,
@@ -13,7 +14,8 @@ window.PRODUCTS = [
     "name": "낚시 릴 감기기 라인 와인더 감기 도구 릴줄 정리 용품 업그레이드 세트 [미터 카운터 포함]",
     "image": "images/13700267842.jpg",
     "url": "https://smartstore.naver.com/itstem123123/products/13700267842",
-    "keywords": "낚시 릴 감기기 라인 와인더 감기 도구 릴줄 정리 용품 업그레이드 세트 [미터 카운터 포함]"
+    "keywords": "낚시 릴 감기기 라인 와인더 감기 도구 릴줄 정리 용품 업그레이드 세트 [미터 카운터 포함]",
+    "coupang_url": ""
   },
   {
     "internal_no": 3,
@@ -21,7 +23,8 @@ window.PRODUCTS = [
     "name": "서버랙 벽걸이형 네트워크 랙 4 U 디럭스 530x400x240 벽걸이형 바닥형",
     "image": "images/13700268351.jpg",
     "url": "https://smartstore.naver.com/itstem123123/products/13700268351",
-    "keywords": "서버랙 벽걸이형 네트워크 랙 4 U 디럭스 530x400x240 벽걸이형 바닥형"
+    "keywords": "서버랙 벽걸이형 네트워크 랙 4 U 디럭스 530x400x240 벽걸이형 바닥형",
+    "coupang_url": ""
   },
   {
     "internal_no": 4,
@@ -29,7 +32,8 @@ window.PRODUCTS = [
     "name": "윤활유 내마모 테스트기 연료 기름 구리스 공업소 280W 디지털",
     "image": "images/13701069188.jpg",
     "url": "https://smartstore.naver.com/itstem123123/products/13701069188",
-    "keywords": "윤활유 내마모 테스트기 연료 기름 구리스 공업소 280W 디지털"
+    "keywords": "윤활유 내마모 테스트기 연료 기름 구리스 공업소 280W 디지털",
+    "coupang_url": ""
   },
   {
     "internal_no": 5,
@@ -37,6 +41,7 @@ window.PRODUCTS = [
     "name": "야외 베란다 철제 방수 청소도구 정리함",
     "image": "images/13701070024.jpg",
     "url": "https://smartstore.naver.com/itstem123123/products/13701070024",
+    "coupang_url": "",
     "keywords": "야외 베란다 철제 방수 청소도구 정리함"
   },
   {
@@ -45,6 +50,7 @@ window.PRODUCTS = [
     "name": "야외 테라스 정원 테이블 세트 플라스틱 우드 휴게 공간용",
     "image": "images/13701070612.jpg",
     "url": "https://smartstore.naver.com/itstem123123/products/13701070612",
+    "coupang_url": "",
     "keywords": "야외 테라스 정원 테이블 세트 플라스틱 우드 휴게 공간용"
   },
   {
@@ -53,6 +59,7 @@ window.PRODUCTS = [
     "name": "산업용 미스트 선풍기 가습 냉풍기 강력 수냉식 업소용 야외용",
     "image": "images/13702375692.jpg",
     "url": "https://smartstore.naver.com/itstem123123/products/13702375692",
+    "coupang_url": "",
     "keywords": "산업용 미스트 선풍기 가습 냉풍기 강력 수냉식 업소용 야외용"
   },
   {
@@ -61,7 +68,8 @@ window.PRODUCTS = [
     "name": "플라스틱 대형 직사각형 수납함 뚜껑 포함 물류용 박스 755 박스 [815x570x505 mm] 옐로우 뚜껑 없음",
     "image": "images/13707333359.jpg",
     "url": "https://smartstore.naver.com/itstem123123/products/13707333359",
-    "keywords": "플라스틱 대형 직사각형 수납함 뚜껑 포함 물류용 박스 755 박스 [815x570x505 mm] 옐로우 뚜껑 없음"
+    "keywords": "플라스틱 대형 직사각형 수납함 뚜껑 포함 물류용 박스 755 박스 [815x570x505 mm] 옐로우 뚜껑 없음",
+    "coupang_url": ""
   },
   {
     "internal_no": 9,
@@ -69,7 +77,8 @@ window.PRODUCTS = [
     "name": "회전 서가 원형 흰색 선반 슬라이딩 회전책장 잡지 공간분리 원목업그레이드카멜 3단 확대 배송입",
     "image": "images/13720636322.jpg",
     "url": "https://smartstore.naver.com/itstem123123/products/13720636322",
-    "keywords": "회전 서가 원형 흰색 선반 슬라이딩 회전책장 잡지 공간분리 원목업그레이드카멜 3단 확대 배송입"
+    "keywords": "회전 서가 원형 흰색 선반 슬라이딩 회전책장 잡지 공간분리 원목업그레이드카멜 3단 확대 배송입",
+    "coupang_url": ""
   },
   {
     "internal_no": 10,
@@ -77,6 +86,7 @@ window.PRODUCTS = [
     "name": "업소용 닭털 제거기계 자동 제모기 세척 처리장비 분무형 전동 깃털 탈모기 40L스탠다드핏 탈비둘기 탈계불가",
     "image": "images/13720639918.jpg",
     "url": "https://smartstore.naver.com/itstem123123/products/13720639918",
-    "keywords": "업소용 닭털 제거기계 자동 제모기 세척 처리장비 분무형 전동 깃털 탈모기 40L스탠다드핏 탈비둘기 탈계불가"
+    "keywords": "업소용 닭털 제거기계 자동 제모기 세척 처리장비 분무형 전동 깃털 탈모기 40L스탠다드핏 탈비둘기 탈계불가",
+    "coupang_url": ""
   }
 ];
