5405482e1c0d3fb97b85671b831c2a3705b535a5
itstem123 <denroa123123@gmail.com>
2026-09-07 23:22:25 +0900

    영상 속 상품찾기 26번 products.js 자동 갱신

--- products.js
+++ products.js
@@ -247,7 +247,11 @@ window.PRODUCTS = [
     "image": "images/13726818363.jpg",
     "url": "https://smartstore.naver.com/itstem123123/products/13726818363",
     "coupang_url": "",
-    "keywords": "전동 전정기 헤지트리머 대용량 고출력 원예 가지치기 앵글 그라인더 헤지 트리머(30cm)"
+    "keywords": "전동 전정기 헤지트리머 대용량 고출력 원예 가지치기 앵글 그라인더 헤지 트리머(30cm)",
+    "smartstore_url": "https://smartstore.naver.com/itstem123123/products/13726818363",
+    "market_display": "both",
+    "show_smartstore": true,
+    "show_coupang": true
   },
   {
     "internal_no": 27,
